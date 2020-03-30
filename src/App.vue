eedc7aefe0c2b615dd3fa7a62d876207ad2921e9
Sean Corcoran <corcoransean28@gmail.com>
2020-03-30 15:05:20 +0100

    added todo component and linked to app with props

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Todos v-bind:todos="todos" />
+  </div>
 </template>
 
 <script>
+import Todos from "./components/Todos";
+
 export default {
   name: "app",
-  components: {}
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "todo 1",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "todo 2",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "todo 3",
+          completed: false
+        }
+      ]
+    };
+  }
 };
 </script>
 
---
+++ src/components/Todos.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Todos</h1>
+    <ul v-bind:key="todo.id" v-for="todo in todos">
+      <li>{{todo.title}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todos",
+  props: ["todos"]
+};
+</script>
+
+<style scoped>
+</style>
